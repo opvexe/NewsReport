215fbb42875bd1b5742b2fb9887f304057b86ce5
shumintao <tao_shuminX@foxmail.com>
2018-02-05 10:46:59 +0800

    no message

---
+++ NewsReport/NewsReport/Class/Utils/voice manager/record view/NRSoundRecorder.h
@@ -0,0 +1,13 @@
+//
+//  NRSoundRecorder.h
+//  NewsReport
+//
+//  Created by Facebook on 2018/2/5.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NRSoundRecorder : NSObject
+
+@end
---
+++ NewsReport/NewsReport/Class/Utils/voice manager/record view/NRSoundRecorder.m
@@ -0,0 +1,13 @@
+//
+//  NRSoundRecorder.m
+//  NewsReport
+//
+//  Created by Facebook on 2018/2/5.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+#import "NRSoundRecorder.h"
+
+@implementation NRSoundRecorder
+
+@end
