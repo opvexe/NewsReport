4c39a06c25bdc994f24f9c01f8c677dc467d3d7c
shumintao <tao_shuminX@foxmail.com>
2018-02-05 17:22:09 +0800

    no message

---
+++ NewsReport/NewsReport/Class/Common/BaseModel/NRConversation.h
@@ -0,0 +1,13 @@
+//
+//  NRConversation.h
+//  NewsReport
+//
+//  Created by Facebook on 2018/2/5.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NRConversation : NSObject
+
+@end
---
+++ NewsReport/NewsReport/Class/Common/BaseModel/NRConversation.m
@@ -0,0 +1,13 @@
+//
+//  NRConversation.m
+//  NewsReport
+//
+//  Created by Facebook on 2018/2/5.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+#import "NRConversation.h"
+
+@implementation NRConversation
+
+@end
