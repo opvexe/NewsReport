923154e231ec6685d18afc10fbe23c4c73b917f9
shumintao <tao_shuminX@foxmail.com>
2018-03-13 08:52:30 +0800

    no message

--- NewsReport/NewsReport/Class/Utils/Tools/NRCoreToolCenter.h
+++ NewsReport/NewsReport/Class/Utils/Tools/NRCoreToolCenter.h
@@ -132,4 +132,34 @@ extern void DismissHud(void);
 extern void JumpAnimation (UIView *view ,NSTimeInterval duration,float height);
 
 
+#pragma mark 使用方法
+/*
+ 
+ NSString *total1 = @"单纯改变颜色,不做其他设置,单纯改变颜色";
+ NSArray *subArray1 = @[@"单纯",@"其他"];
+ 
+ self.changeColorString = [NRCoreToolCenter ls_changeCorlorWithColor:[UIColor redColor] TotalString:total1 SubStringArray:subArray1];
+ 
+ // *******单纯改变字间距*********
+ 
+ NSString *total2 = @"单纯改变字间距,此时为10";
+ self.changeSpaceString = [NRCoreToolCenter ls_changeSpaceWithTotalString:total2 Space:10.0];
+ 
+ // *******单纯改行间距********
+ 
+ NSString *total3 = @"单纯改变行间距，此时行间距为10。单纯改变行间距，此时行间距为10。单纯改变行间距，此时行间距为10。单纯改变行间距，此时行间距为10。单纯改变行间距，此时行间距为10。";
+ self.changeLineSpaceString = [NRCoreToolCenter ls_changeLineSpaceWithTotalString:total3 LineSpace:10.0];
+ 
+ // ******改变某些文字的颜色,并单独设置其字体*****
+ 
+ NSString *total4 = @"改变某些文字的颜色,并单独设置其字体";
+ self.changeFontColorString = [NRCoreToolCenter ls_changeFontAndColor:[UIFont boldSystemFontOfSize:20] Color:[UIColor redColor] TotalString:total4 SubStringArray:@[@"某些",@"设置"]];
+ 
+ // *******同时改行间距和字间距********
+ 
+ NSString *total5 = @"同时改行间距和字间距，此时行间距为10，字间距为5。同时改行间距和字间距，此时行间距为10，字间距为5。同时改行间距和字间距，此时行间距为10，字间距为5。";
+ self.changeLineAndSpaceColorString = [NRCoreToolCenter ls_changeLineAndTextSpaceWithTotalString:total5 LineSpace:10.0 textSpace:5.0];
+
+ 
+ */
 @end
